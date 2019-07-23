c23486134de62f1aa5c6e9cd29e15670e20998e4
Shava <salvador.gonzalez-nolasco@capgemini.com>
2019-07-22 21:39:11 -0500

    initial commit

---
+++ virtual-wallet/src/main/java/mx/uvm/edu/virtualwallet/ApplicationStarter.java
@@ -0,0 +1,40 @@
+/*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* 
+*
+* Nombre de archivo: ApplicationStarter.java
+* Autor: salvgonz
+* Fecha de creación: 22/07/2019
+*/
+
+package mx.uvm.edu.virtualwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Class ApplicationStarter.
+ */
+@SpringBootApplication
+public class ApplicationStarter {
+
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationStarter.class, args);
+  }
+}
